8736e4143354d04dc4ede979358d7e0e5bd0dba6
Lars Werner <lars@werner.no>
2019-10-08 21:18:30 +0200

    test2

---
+++ ARSoft.Tools.Net/nuget.bat
@@ -0,0 +1,3 @@
+﻿nuget.exe sources Add -Name "WernersNuGet" -Source "https://pkgs.dev.azure.com/hesten/_packaging/WernersNuGet/nuget/v3/index.json"
+nuget.exe push -Source "WernersNuGet" -ApiKey AzureDevOps ARSoft.Tools.Net.3.0.0.nupkg
+rem Velg den pakka som du har lagd (bruker assambly info for versjon osv osv)
